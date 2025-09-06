73e21ca8bae93d828d655d33a2b8df162dfc05f3
Matrix030 <rishigharat@gmail.com>
2025-09-06 12:13:57 -0400

    made a different file for incoming json data type struct

--- main.go
+++ main.go
@@ -1,30 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"time"
 
-
-URL := "https://raw.githubusercontent.com/SimplifyJobs/New-Grad-Positions/refs/heads/dev/.github/scripts/listings.json"
-type jobStruct []struct {
-		Source      string   `json:"source"`
-		CompanyName string   `json:"company_name"`
-		ID          string   `json:"id"`
-		Title       string   `json:"title"`
-		Active      bool     `json:"active"`
-		DateUpdated int      `json:"date_updated"`
-		DatePosted  int      `json:"date_posted"`
-		URL         string   `json:"url"`
-		Locations   []string `json:"locations"`
-		CompanyURL  string   `json:"company_url"`
-		IsVisible   bool     `json:"is_visible"`
-		Sponsorship string   `json:"sponsorship"`
-		Category    string   `json:"category,omitempty"`
-		Degrees     []any    `json:"degrees,omitempty"`
-	}
+	"github.com/Matrix030/simplify_jobs_cli/internal/simplifyapi"
+)
 
 func main() {
-	//TODO- get JSON from URL
-	//PARSE the JSON in a struct
-
-
-		 
+	simplifyClient := simplifyapi.NewClient(5 * time.Minute)
+	cfg := &clientConfig{
+		simplifyapiClient: simplifyClient,
+	}
 }
