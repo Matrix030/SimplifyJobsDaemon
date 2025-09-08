1b2a6ef2d4325d2af221c3d40cf10273bb9c8fb0
Matrix030 <rishigharat@gmail.com>
2025-09-08 19:33:04 -0400

    starting with a new module

---
+++ compare_jobs.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	api "github.com/Matrix030/simplify_jobs_cli/internal/simplifyapi"
+)
+
+func compareJobs(oldJobs, newJobs api.Jobs) api.Jobs {
+	//this compares the old and the new jobs
+	//this also filters out based on sponsorships
+	return api.Jobs{}
+}
