4e9cb1301420a1ee366c649842ecb0bde5c4c045
Matrix030 <rishigharat@gmail.com>
2025-09-08 16:37:43 -0400

    updated files

--- start_client.go
+++ start_client.go
@@ -1,32 +1,47 @@
 package main
 
 import (
+	"fmt"
 	api "github.com/Matrix030/simplify_jobs_cli/internal/simplifyapi"
+	"time"
 )
 
 type config struct {
 	jobClient api.Client
 }
 
-// func startClient(cfg *config) {
-//
-// 	// ticker := time.NewTicker(30 * time.Minute)
-// 	// defer ticker.Stop()
-// 	//
-// 	//running once on startup
-// 	// fetchAndProcessJobs(cfg)
-// 	// for {
-// 	// 	select {
-// 	// 	case <-ticker.C:
-// 	// 		fetchAndProcessJobs(cfg)
-// 	// 	}
-// 	// }
-// }
+func startClient(cfg *config) {
+	ticker := time.NewTicker(30 * time.Minute)
+	defer ticker.Stop()
 
-// func fetchAndProcessJobs(cfg *config) {
-// 	jobs, err := cfg.jobClient.GetJobData()
-// 	if err != nil {
-// 		fmt.Println("There was an error %v\n", err)
-// 		return
-// 	}
-// }
+	//running once on startup
+
+	oldJobs, err := fetchAndProcessJobs(cfg)
+	if err != nil {
+		fmt.Printf("An error occurred while fetch the job %v\n", err)
+		return
+	}
+
+	for {
+		<-ticker.C
+		newJobs, err := fetchAndProcessJobs(cfg)
+		if err != nil {
+			fmt.Printf("An Error Occurred while getting the new jobs\n")
+			return
+		}
+
+		oldJobs = newJobs
+
+		//comparison logic goes here
+	}
+
+}
+
+func fetchAndProcessJobs(cfg *config) (api.Jobs, error) {
+	jobs, err := cfg.jobClient.GetJobData()
+	if err != nil {
+		fmt.Printf("There was an error %v\n", err)
+		return api.Jobs{}, err
+	}
+	return jobs, nil
+}
