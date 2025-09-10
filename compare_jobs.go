f8cda3871977e2a303632ce39640c69bd77b0a47
Matrix030 <rishigharat@gmail.com>
2025-09-10 01:29:26 -0400

    done with compare jobs

--- compare_jobs.go
+++ compare_jobs.go
@@ -5,7 +5,12 @@ import (
 )
 
 func compareJobs(oldJobs, newJobs api.Jobs) api.Jobs {
-	//this compares the old and the new jobs
-	//this also filters out based on sponsorships
-	return api.Jobs{}
+	var newJobsOnly api.Jobs
+	for len(oldJobs) > 0 && len(newJobs) > 0 && oldJobs[len(oldJobs)-1].ID != newJobs[len(newJobs)-1].ID {
+		poppedJob := newJobs[len(newJobs)-1]
+		newJobs = newJobs[:len(newJobs)-1]
+
+		newJobsOnly = append(newJobsOnly, poppedJob)
+	}
+	return newJobsOnly
 }
