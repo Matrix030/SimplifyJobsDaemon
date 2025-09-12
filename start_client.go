e8ae8f11c04e4104441e4804a4eb69ee0c96841a
Matrix030 <rishigharat@gmail.com>
2025-09-12 18:15:07 -0400

    updated for final

--- start_client.go
+++ start_client.go
@@ -16,43 +16,78 @@ func startClient(cfg *config) {
 	ticker := time.NewTicker(30 * time.Minute)
 	defer ticker.Stop()
 
-	//running once on startup
+	// Running once on startup
 	oldJobsData, err := utils.LoadExistingJobs()
 	if err != nil {
 		fmt.Printf("File not found: %v\n", err)
 		// Fetch new jobs since file doesn't exist
 		oldJobs, err = fetchAndProcessJobs(cfg)
 		if err != nil {
-			fmt.Printf("An error occurred while fetching the jobs %v\n", err)
-			return // or handle error appropriately
-		}
-		// Only write if fetch succeeded
-		utils.JsonFileWriter(oldJobs)
-	} else {
-		oldJobs = oldJobsData
-	}
-
-	for {
-		<-ticker.C
-		newJobs, err := fetchAndProcessJobs(cfg)
-		if err != nil {
-			fmt.Printf("An Error Occurred while getting the new jobs\n")
+			fmt.Printf("An error occurred while fetching the jobs: %v\n", err)
 			return
 		}
+		// Write the initial jobs to file
+		err = utils.JsonFileWriter(oldJobs)
+		if err != nil {
+			fmt.Printf("Error writing initial jobs to file: %v\n", err)
+			return
+		}
+	} else {
+		oldJobs = oldJobsData
+		fmt.Printf("Loaded %d existing jobs from file\n", len(oldJobs))
+	}
 
-		oldJobs = newJobs
+	fmt.Println("Starting job monitoring... Press Ctrl+C to stop")
 
+	// Main monitoring loop
+	for {
+		<-ticker.C
+		fmt.Println("Checking for new jobs...")
+
+		newJobs, err := fetchAndProcessJobs(cfg)
+		if err != nil {
+			fmt.Printf("An error occurred while getting new jobs: %v\n", err)
+			continue // Don't exit, just skip this iteration
+		}
+
+		// Compare old jobs with new jobs to find newly added ones
 		newJobSlice := compareJobs(oldJobs, newJobs)
-		//TODO:
-		utils.SendNotification(newJobSlice)
+
+		// Always write the new jobs file (even if empty)
+		err = utils.WriteNewJobsOnly(newJobSlice)
+		if err != nil {
+			fmt.Printf("Error writing new jobs file: %v\n", err)
+		}
+
+		if len(newJobSlice) > 0 {
+			fmt.Printf("Found %d new jobs!\n", len(newJobSlice))
+
+			// Send notification for new jobs
+			err = utils.SendNotification(newJobSlice)
+			if err != nil {
+				fmt.Printf("Error sending notification: %v\n", err)
+			}
+
+			// Update the stored jobs file with all current jobs
+			err = utils.JsonFileWriter(newJobs)
+			if err != nil {
+				fmt.Printf("Error updating jobs file: %v\n", err)
+			}
+		} else {
+			fmt.Println("No new jobs found")
+		}
+
+		// Update oldJobs for next comparison
+		oldJobs = newJobs
 	}
 }
 
 func fetchAndProcessJobs(cfg *config) (api.Jobs, error) {
 	jobs, err := cfg.jobClient.GetJobData()
 	if err != nil {
-		fmt.Printf("There was an error %v\n", err)
-		return api.Jobs{}, err
+		return api.Jobs{}, fmt.Errorf("failed to fetch job data: %w", err)
 	}
+
+	fmt.Printf("Fetched %d total jobs\n", len(jobs))
 	return jobs, nil
 }
